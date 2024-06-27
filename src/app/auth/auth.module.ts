faeee7c3daf0abd7c6ffefc9080626dab363a6ab
Devaswathi <swathidevasenapathy@gmail.com>
2024-06-27 12:47:16 +0530

    Form validation and scss changes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { FormsModule , NgModel, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgModule,
+    NgModel
   ]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { RegisterComponent } from "../register/register.component";
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
-    imports: [ButtonModule, IconFieldModule, InputTextModule, InputIconModule, RegisterComponent , NgClass]
+    imports: [ ButtonModule, IconFieldModule, InputTextModule, InputIconModule, RegisterComponent , NgClass , FormsModule , NgIf ]
 })
 export class LoginComponent {
+
+    onSubmit(form: NgForm): void {
+        console.log(form); 
+      }
 
 }
