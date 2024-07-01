de6a95de50690e77ed706272bb94fd7b4c7e0782
Devaswathi <swathidevasenapathy@gmail.com>
2024-07-01 15:16:25 +0530

    Add register api

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthRoutingModule } from './auth-routing.module';
 import { FormsModule , NgModel, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './service/user-service';
+import { MessageService } from 'primeng/api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    NgModule,
-    NgModel
-  ]
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    UserService,
+    MessageService 
+  ],
 })
 export class AuthModule { }
